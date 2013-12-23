e5d34ce2036836db2ec75ca7d65ee895db84b764
digitalvirtuoso <fearchaos@gmail.com>
2013-12-23 00:26:28 -0600

    Correcting strange merge bug (+more)
    Adding commented out line for the new input field (auth)

--- src/com/companyx/android/appx/RecipeLoader.java
+++ src/com/companyx/android/appx/RecipeLoader.java
@@ -18,7 +18,6 @@ import com.companyx.android.appx.RecipeDatabase.RecipeTime;
 // Recipe Reader
 // @author Adam Hackbarth <adam.hackbarth@gmail.com>
 public class RecipeLoader {
-<<<<<<< HEAD
 	InputStream inputStream;
 	RecipeDatabase recipeDatabase;
 
@@ -124,7 +123,7 @@ public class RecipeLoader {
 					dirList.add(new RecipeDirection(s));
 				}
 
-				Recipe newRecipe = new Recipe(recipeNumber, title, riList, dirList, timeC, serveSize, diff);  //auth
+				Recipe newRecipe = new Recipe(recipeNumber, title, auth, riList, dirList, timeC, serveSize, diff);
 				recipeDatabase.addRecipe(newRecipe);
 
 				recipeNumber++; // increment recipe numbering system
@@ -142,120 +141,4 @@ public class RecipeLoader {
 
 		scanner.close();
 	}
-=======
-        InputStream inputStream;
-        RecipeDatabase recipeDatabase;
-
-        // constructor
-        RecipeLoader(InputStream inputStream, RecipeDatabase recipeDatabase) {
-                this.inputStream = inputStream;
-                this.recipeDatabase = recipeDatabase;
-        }
-
-        public void loadData() {
-
-                /* Loading Raw Data from File Location into Scanner */
-                Scanner scanner = new Scanner(inputStream);
-                scanner.useDelimiter(System.getProperty("line.separator"));
-
-                String bV = "\\b\\d:\\D"; // New Entry DISCOVERY Text
-                String INPUT = scanner.next();
-                String keyLine = "";
-                int recipeNumber = 0;
-
-                while (scanner.hasNext()) {
-
-                        if (INPUT.matches(bV)) {
-                                keyLine = INPUT; // saving the recipe key line
-                                String initVal = scanner.next(); // Starting Point Holder
-                                String regex = "\\b  +";
-                                String blank = "";
-                                String title = "";
-                                INPUT = initVal;
-                                // Difficulty Placeholder.
-                                byte diff = 0;
-
-                                // Storing Recipe title
-                                while (scanner.hasNext()) {
-
-                                        if (INPUT.equals(bV)) {
-                                                INPUT = scanner.next();
-                                        } else {
-                                                title = INPUT;
-                                                INPUT = scanner.next();
-                                                break;
-                                        }
-                                }
-                                // Time Split into Int - then Short into the RecipeTime
-                                String timeL[] = INPUT.split("\\:");
-                                int pr = Integer.valueOf(timeL[0].trim());
-                                int ipr = Integer.valueOf(timeL[1].trim());
-                                int c = Integer.valueOf(timeL[2].trim());
-
-                                RecipeTime timeC = new RecipeTime((short)pr, (short)ipr, (short)c);
-
-                                // Serving size byte (serveSize)
-                                int q = Integer.valueOf(timeL[3].trim());
-                                byte serveSize = (byte) q;
-
-                                INPUT = scanner.next();
-
-                                ArrayList<String> ingredListArray = new ArrayList<String>();
-                                List<String> directList = new ArrayList<String>();
-                                // Grabbing Recipe Ingredients
-                                while (scanner.hasNext()) {
-                                        ingredListArray.add(INPUT);
-                                        INPUT = scanner.next();
-                                        if (INPUT.matches("\\b_.*")) {
-                                                break;
-                                        }
-                                }
-                                // Grabbing Recipe Directions/Instructions
-                                String dirL[] = INPUT.substring(1, INPUT.length()).split("\\:");
-                                directList = Arrays.asList(dirL);
-
-                                while (scanner.hasNext()) {
-                                        if (INPUT.matches(bV)) {
-                                                break;
-                                        } else {
-                                                INPUT = scanner.next();
-                                        }
-                                }
-                                // Separating the Ingredients for Database import
-                                List<RecipeIngredient> riList = new
-                                ArrayList<RecipeIngredient>();
-                                for (String z : ingredListArray) {
-                                        regex = "\\:"; 
-                                        Pattern p = Pattern.compile(regex);
-                                        String[] ami = p.split(z.toLowerCase());
-                                        RecipeIngredient newIngredient = new
-                                        RecipeIngredient(ami[0].trim(), ami[1].trim(), ami[2].trim(), ami[3].trim());
-                                        riList.add(newIngredient);
-                                }
-                        
-                                List<RecipeDirection> dirList = new
-                                ArrayList<RecipeDirection>();
-                                for (String s : directList){
-                                        dirList.add(new RecipeDirection(s));
-                                }
-
-                                Recipe newRecipe = new Recipe(recipeNumber, title, riList, dirList, timeC, serveSize, diff);
-                                recipeDatabase.addRecipe(newRecipe);
-
-                                recipeNumber++; // increment recipe numbering system
-
-                        } else {
-                                while (scanner.hasNext()) {
-                                        if (INPUT.equals(bV)) {
-                                                break;
-                                        }
-                                        INPUT = scanner.next();
-
-                                }
-                        }
-                }
-
-                scanner.close();
-        }
->>>>>>> origin/master
 }
