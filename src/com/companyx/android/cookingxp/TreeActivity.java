7eb96e5d2ea3b055fe75c537afebc02b07f71954
JLChin <jameslchin@gmail.com>
2014-01-11 15:02:06 -0800

    Game progress

--- src/com/companyx/android/cookingxp/TreeActivity.java
+++ src/com/companyx/android/cookingxp/TreeActivity.java
@@ -1,5 +1,9 @@
 package com.companyx.android.cookingxp;
 
+import java.util.List;
+
+import com.companyx.android.cookingxp.GameData.Tree;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ImageView;
@@ -11,7 +15,7 @@ import android.widget.TableLayout;
  * @author James Chin <jameslchin@gmail.com>
  */
 public class TreeActivity extends BaseActivity {
-	
+	GameData gameData;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +26,15 @@ public class TreeActivity extends BaseActivity {
 	}
 	
 	private void constructTree() {
+		gameData = GameData.getInstance(this);
+		List<Tree> treeList = gameData.getTrees();
+		
 		TableLayout tableTree = (TableLayout) findViewById(R.id.table_tree);
 		ImageView imgView = new ImageView(this);
 		imgView.setImageResource(R.drawable.ic_box_locked);
 		
+		// TODO replace with live version
+		Tree tree = treeList.get(0);
 		tableTree.addView(imgView);
 	}
 	
