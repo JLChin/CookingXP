4c939648f442cb66f18558a5920b7591e2a0fac0
JLChin <jameslchin@gmail.com>
2014-01-16 10:11:41 -0800

    TreeActivity EdgeView bookmark, added spinner

--- src/com/companyx/android/cookingxp/TreeActivity.java
+++ src/com/companyx/android/cookingxp/TreeActivity.java
@@ -1,5 +1,6 @@
 package com.companyx.android.cookingxp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,10 +16,14 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.companyx.android.cookingxp.GameData.Box;
@@ -34,10 +39,12 @@ import com.companyx.android.cookingxp.RecipeDatabase.Recipe;
 public class TreeActivity extends BaseActivity {
 	// CONSTANTS
 	public static final float DEFAULT_IMAGE_SPACING_RATIO = 0.1f; // ratio of image spacing to screen width
+	public static final int[] TREES = {R.string.game_tree0, R.string.game_tree1, R.string.game_tree2};
 	
 	// VIEW HOLDERS
 	private LinearLayout layoutTree;
 	private RelativeLayout layoutTreeOverlay;
+	private Spinner spinnerTree;
 	
 	// STATE VARIABLES
 	private List<Tree> treeList;
@@ -53,6 +60,7 @@ public class TreeActivity extends BaseActivity {
 		setContentView(R.layout.activity_trees);
 		
 		initialize();
+		initializeSpinner();
 		constructTree(treeList.get(0));
 	}
 	
@@ -68,6 +76,33 @@ public class TreeActivity extends BaseActivity {
 	}
 	
 	/**
+	 * Set up the Tree selection Spinner.
+	 */
+	private void initializeSpinner() {
+		spinnerTree = new Spinner(this);
+		
+		// add selections
+		List<String> trees = new ArrayList<String>();
+		for (int i : TREES)
+			trees.add(getString(i));
+		spinnerTree.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, trees));
+		
+		// attach listener
+		spinnerTree.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				// TODO position = treeId
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
+		
+		layoutTree.addView(spinnerTree);
+	}
+	
+	/**
 	 * Constructs Tree layout from GameData info.
 	 * @param tree the Tree object to construct the layout for.
 	 */
@@ -76,8 +111,11 @@ public class TreeActivity extends BaseActivity {
 		@SuppressWarnings("deprecation")
 		float screenWidthInPixels = getWindowManager().getDefaultDisplay().getWidth();
 		int imgSpacingInPixels = (int) (DEFAULT_IMAGE_SPACING_RATIO * screenWidthInPixels);
-		/*
+		
 		for (int tier = 0; tier < tree.boxHolderMatrix.size(); tier++) {
+			// horizontal break between tiers
+			layoutTree.addView(new View(this), new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, imgSpacingInPixels));
+						
 			// tier container
 			RelativeLayout rl = new RelativeLayout(this);
 			RelativeLayout.LayoutParams paramsRL = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
@@ -133,15 +171,15 @@ public class TreeActivity extends BaseActivity {
 			
 			// tier constructed, add to layout
 			layoutTree.addView(rl);
-			
-			// horizontal break between tiers
-			layoutTree.addView(new View(this), new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, imgSpacingInPixels));
 		}
-		*/
+		
 		// draw edges between Boxes
 		layoutTreeOverlay.addView(new EdgeView(this));
 	}
 	
+	/**
+	 * Custom View class for drawing the path edges between Boxes.
+	 */
 	private static class EdgeView extends View {
 		Paint paint;
 
